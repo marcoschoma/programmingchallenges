20a7339a6458d5ec8bb9d176ac7b116b61b1dd3c
marcos choma <marcos.choma@valor.com.br>
2015-12-30 16:38:50 -0200

    HackerEarth problems

---
+++ NumberOfRs/NumberOrRsProgram.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfRs
+{
+    class NumberOrRsProgram
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Trim();
+            var N = int.Parse(line);
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(MaxRs(Console.ReadLine().Trim()));
+            }
+            Console.ReadKey();
+        }
+
+        static int MaxRs(string input)
+        {
+            int[] a = new int[1000003];
+            int[] dp = new int[1000003];
+
+            int inputLength = input.Length;
+            int countBeforeFlip = 0, maxCountOfRs;
+            for (int i = 0; i < inputLength; i++)
+            {
+                if (input[i] == 'R')
+                {
+                    a[i] = -1;
+                    countBeforeFlip++;
+                }
+                else a[i] = 1;
+            }
+            maxCountOfRs = dp[0] = a[0];
+            for (int i = 1; i < inputLength; i++)
+            {
+                dp[i] = Math.Max(a[i], a[i] + dp[i - 1]);
+                maxCountOfRs = Math.Max(dp[i], maxCountOfRs);
+            }
+            return maxCountOfRs + countBeforeFlip;
+        }
+    }
+}
---
+++ PromNight/PromNightProgram.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromNight
+{
+    class PromNightProgram
+    {
+        static void Main(string[] args)
+        {
+            var line = System.Console.ReadLine().Trim();
+            var T = Int32.Parse(line);
+            for (var i = 0; i < T; i++)
+            {
+                line = System.Console.ReadLine().Trim();
+                var data = line.Split(' ');
+                var M = int.Parse(data[0]);
+                var N = int.Parse(data[1]);
+
+                line = System.Console.ReadLine().Trim();
+                data = line.Split(' ');
+                int[] boys = data.Select(s => int.Parse(s)).OrderBy(s => s).ToArray<int>();
+
+                line = System.Console.ReadLine().Trim();
+                data = line.Split(' ');
+                int[] girls = data.Select(s => int.Parse(s)).OrderBy(s => s).ToArray<int>();
+
+                if (Calcular(boys, girls, M, N))
+                    YES();
+                else
+                    NO();
+            }
+            //Console.ReadKey();
+        }
+
+        private static bool Calcular(int[] boys, int[] girls, int M, int N)
+        {
+            if (M > N)
+            {
+                return false;
+            }
+            else
+            {
+                int j = 0;
+                for (int i = 0; i < M; i++)
+                {
+                    if (j == N)
+                        break;
+
+                    if (girls[i] < boys[i])
+                    {
+                        j++;
+                        continue;
+                    }
+                }
+                return j == M;
+            }
+        }
+
+        private static void NO()
+        {
+            Console.WriteLine("NO");
+        }
+        private static void YES()
+        {
+            Console.WriteLine("YES");
+        }
+    }
+}
