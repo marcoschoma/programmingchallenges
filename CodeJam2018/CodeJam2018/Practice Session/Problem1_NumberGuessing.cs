a32bd3af1921549fb4b0b556347582181085c505
Marcos Choma <marcos.choma@gmail.com>
2018-11-20 20:42:53 -0200

    movendo arquivos

---
+++ CodeJam2018/CodeJam2018/Practice Session/Problem1_NumberGuessing.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace CodeJam2018
+{
+    public class Problem1_NumberGuessing
+    {
+        public static void Main(string[] args)
+        {
+            string file = "sample";
+            using (var reader = Console.In)
+            {
+                using (var writer = Console.Out)
+                {
+                    int T = int.Parse(reader.ReadLine());
+                    for (int i = 0; i < T; i++)
+                    {
+                        var result = Solve(reader, writer);
+                        writer.Flush();
+                    }
+                }
+            }
+        }
+
+        private static int Solve(TextReader reader, TextWriter writer)
+        {
+            var bounds = reader.ReadLine().Split(' ');
+            var lower = int.Parse(bounds[0]);
+            var upper = int.Parse(bounds[1]);
+
+            var line = reader.ReadLine();
+            var guesses = int.Parse(line);
+
+            var guess = (lower + upper) / 2;
+            for (int i = 0; i < guesses; i++)
+            {
+                writer.WriteLine(guess);
+                var judgeAnswer = reader.ReadLine();
+                if (judgeAnswer == "TOO_BIG")
+                {
+                    upper = guess;
+                } else if (judgeAnswer == "TOO_SMALL")
+                {
+                    lower = guess;
+                }
+                else if (judgeAnswer == "WRONG_ANSWER")
+                {
+                    return -1;
+                }
+                else if (judgeAnswer == "CORRECT")
+                {
+                    break;
+                }
+                guess = (lower + upper) / 2;
+            }
+            return guess;
+        }
+    }
+}
---
+++ CodeJam2018/CodeJam2018/Practice Session/Problem2_SenateEvacuation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+
+namespace CodeJam2018
+{
+    public class Problem2_SenateEvacuation
+    {
+        static void Main2(string[] args)
+        {
+            var fileName = "sample";
+            using (var reader = new StreamReader(fileName + ".in"))
+            {
+                using (var writer = new StreamWriter(fileName + ".out"))
+                {
+                    int T = int.Parse(reader.ReadLine());
+                    for (int i = 0; i < T; i++)
+                    {
+                        var parties = reader.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+                        
+                        var result = Solve(parties);
+                        writer.WriteLine("Case #" + (i + 1) + ": " + result);
+                        writer.Flush();
+                    }
+                }
+            }
+        }
+
+        private static int Solve(int[] parties)
+        {
+            var l = new SortedList();
+            //l.Add()
+
+            var total = parties.Sum();
+            return 0;
+        }
+    }
+}
